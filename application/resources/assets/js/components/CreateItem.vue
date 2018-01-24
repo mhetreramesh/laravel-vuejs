e735f5fcc87b428aa1f87f2291ac2221644ab882
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-01-24 16:48:55 +0530

    Basic product setup done with API & VueJS setup
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

---
+++ application/resources/assets/js/components/CreateItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Create An Item</h1>
+    <form v-on:submit.prevent="addItem">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Article Name:</label>
+            <input type="text" class="form-control" v-model="item.name">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Article Price:</label>
+              <input type="text" class="form-control col-md-6" v-model="item.price" />
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Add Article</button>
+          <router-link :to="{ name: 'DisplayItem' }" class="btn btn-default">Cancel</router-link>
+        </div>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+        return{
+          item:{}
+        }
+    },
+    methods: {
+      addItem(){
+        let uri = 'http://pim.tradebyte.lcl/items';
+        this.axios.post(uri, this.item).then((response) => {
+          this.$router.push({name: 'DisplayItem'})
+        })
+    }
+  }
+}
+</script>
---
+++ application/ssh_docker.sh
@@ -0,0 +1,9 @@
+if [ $1 = "mysql" ]; then
+    docker exec -it rameshtradebyte_database_1 mysql -umarammat -pmarammat marammat
+elif [ $1 = "nginx" ]; then
+    docker exec -it rameshtradebyte_web_1 bash
+elif [ $1 = "php" ]; then
+    docker exec -it rameshtradebyte_app_1 bash
+else 
+    echo "Please pass container as an argument(mysql/nginx/php)."
+fi        
